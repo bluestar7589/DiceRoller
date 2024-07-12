8003e2424f12717c557351af6963c6de4ef4e75a
bluestar7589 <bluestar7589@yahoo.com>
2024-07-11 17:32:09 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DiceRoller
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
